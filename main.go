9f5605b32f0d322d35d509a98376173ce620d808
stefan <stmu@gmx.net>
2016-03-02 21:48:35 +0100

    use direct

--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
-import "github.com/ninjasphere/go-ninja/support"
+import (
+	"driver-block/arduino"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/ninjasphere/go-ninja/config"
+	"github.com/ninjasphere/go-ninja/support"
+)
 
 func main() {
 
-	if _, err := NewDriver(); err != nil {
-		log.Errorf("Failed to create driver: %s", err)
-		return
+	var path = "/dev/ttyO1"
+	var speed = 9600
+
+	port, err := arduino.Connect(path, speed)
+
+	if err != nil {
+		log.Fatalf("Couldn't connect to arduino: %s", err)
 	}
 
-	support.WaitUntilSignal()
+	version, err := port.GetVersion()
+
+	if err != nil {
+		log.Warningf("Failed to get version from arduino. Continuing anyway. #YOLO.")
+	}
+
+	if version != requiredVersion {
+		log.Warningf("Unknown arduino version. Expected:%s Got: %s", requiredVersion, version)
+	}
+
+	NewLight(d, 1007, "Nina's Eyes", port)
+	NewLight(d, 999, "Status Light", port)
+
+	go func() {
+		for message := range port.Incoming {
+			spew.Dump("incoming", message)
+		}
+	}()
+
+	return nil
 }
