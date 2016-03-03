80bfa5bb57afe0d142574535c8d255f38db44289
stefan <stmu@gmx.net>
2016-03-03 21:19:50 +0100

    .

--- main.go
+++ main.go
@@ -23,9 +23,9 @@ func main() {
 		fmt.Printf("Failed to get version from arduino. Continuing anyway. #YOLO.")
 	}
 
-	if version != requiredVersion {
-		fmt.Printf("Unknown arduino version. Expected:%s Got: %s", requiredVersion, version)
-	}
+	// if version != requiredVersion {
+	// 	fmt.Printf("Unknown arduino version. Expected:%s Got: %s", requiredVersion, version)
+	// }
 
 	// NewLight(d, 1007, "Nina's Eyes", port)
 	// NewLight(d, 999, "Status Light", port)
