4ccf2f55b0db7b417aedc1eb22550c7b7f9bb131
stefan <stmu@gmx.net>
2016-03-04 22:51:27 +0100

    .

--- main.go
+++ main.go
@@ -27,8 +27,8 @@ func main() {
 	// 	fmt.Printf("Unknown arduino version. Expected:%s Got: %s", requiredVersion, version)
 	// }
 
-	 NewLight(d, 1007, "Nina's Eyes", port)
-	 NewLight(d, 999, "Status Light", port)
+	//  NewLight(d, 1007, "Nina's Eyes", port)
+	//  NewLight(d, 999, "Status Light", port)
 
 	go func() {
 		for message := range port.Incoming {
